2b68a49eed6d828a73118d6a834f24f37a6916a7
Jaweds11 <2400031055@kluniversity.in>
2025-08-19 14:58:48 +0530

    first commit

---
+++ ex1.js
@@ -0,0 +1,26 @@
+var city ="London";
+let country = "UK";
+const pi =3.14;
+console.log(country)
+console.log(city)
+let a1 =100
+let a2 =200
+console.log(a1 + a2)
+let sum = 5+3;
+let isEqual = (sum == 8);
+console.log(isEqual)
+let assignVar = 10;
+assignVar += 5;
+console.log(assignVar)
+let str = "Hello";
+let num =42;
+let obj = {name: "Alice", age: 30};
+let arr = [1, 2, 3, 4, 5];
+console.log(str, num, obj, arr);
+let attendance =80+5;
+let Eligibleforexam = (attendance >= 75);
+console.log("Eligible for exam ?",Eligibleforexam);
+function greetStudent(name) {
+    return "Hello, " + name + " to KL !";
+}
+console.log(greetStudent("JAWED"));
